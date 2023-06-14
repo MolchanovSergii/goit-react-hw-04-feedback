fa260c9ef707ba63c30dcb1741eb013b6a0b2f32
MolchanovSergii <molchanse@gmail.com>
2023-06-14 21:45:31 +0300

    start

---
+++ src/src/components/Feedback/FeedbackOptions.js
@@ -0,0 +1,25 @@
+import css from './Feedback.module.css';
+import PropTypes from 'prop-types';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <>
+      {options.map(option => (
+        <button
+          className={css.button_feedback}
+          key={option}
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option}
+        </button>
+      ))}
+    </>
+  );
+};
+
+export default FeedbackOptions;
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
